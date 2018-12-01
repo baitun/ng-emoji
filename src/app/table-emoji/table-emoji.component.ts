b59cb8851cdfd86ea12ea631bb3a21f29c9374cc
Yury Savin <savinyurii@gmail.com>
2018-12-01 19:44:49 +0800

    ✨ Different buttons depending on the page

--- src/app/table-emoji/table-emoji.component.ts
+++ src/app/table-emoji/table-emoji.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Emoji } from '../emoji.model';
 import { EmojiService } from '../emoji.service';
 
@@ -8,14 +9,22 @@ import { EmojiService } from '../emoji.service';
   styleUrls: ['./table-emoji.component.css'],
 })
 export class TableEmojiComponent {
-  constructor(private emojiService: EmojiService) {}
+  constructor(
+    private emojiService: EmojiService,
+    private route: ActivatedRoute
+  ) {}
   @Input() search_value: string;
-  page: string = 'all';
+  path: string = 'all';
   emojis: Emoji[];
   getEmojis(): void {
     this.emojiService.getEmojis().subscribe(emojis => (this.emojis = emojis));
   }
   ngOnInit() {
+    this.path = this.route.snapshot.routeConfig.path || 'all';
+    console.log('path=', this.path);
     this.getEmojis();
+  }
+  onFavoriteClick(d) {
+    console.log(d);
   }
 }
